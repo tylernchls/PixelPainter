669048ab7447cfd4247498d18f3989f994ca4773
Tyler Nichols <tylernchls@gmail.com>
2016-12-15 19:57:07 -1000

    need to google how to make column of divs into square

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,28 +1,13 @@
 function setGrid(num) {
 
-  var row = document.getElementById("grid");
+
   var column = document.getElementById("grid");
 
   for (var i = 0; i < num; i++) {
-    var div = document.createElement("div");
-    div.id = 'div' + i;
-    row.appendChild(div);
-    div.style.float = "left";
-    // div.style.outLine = "1px";
-    div.style.backgroundColor = "red";
-    div.style.height = "20px";
-    div.style.width = "20px";
-
-    div.addEventListener("mouseover", function() {
-      this.style.backgroundColor = " blue";
-      console.log(this);
-    });
-
     for (var x = 0; x < num; x++) {
       var div = document.createElement("div");
       div.id = 'div' + x;
       column.appendChild(div);
-      div.style.float = "left";
       // div.style.outLine = "1px";
       div.style.backgroundColor = "orange";
       div.style.height = "20px";
@@ -35,10 +20,11 @@ function setGrid(num) {
 
     }
   }
-
-
 }
-setGrid(30);
+
+
+
+setGrid(10);
 
 
 
