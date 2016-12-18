6b32ca5aaae10f6262cb7ba88f59fe10659ccc97
Tyler Nichols <tylernchls@gmail.com>
2016-12-17 19:29:13 -1000

    added second grid for colorSwatch

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,6 +1,5 @@
 function setGrid(n, m) {
 
-
   var grid = document.getElementById("grid");
   grid.setAttribute('style',`width: ${n*20}px; height: ${m*20}px;` );
 
@@ -13,9 +12,8 @@ function setGrid(n, m) {
       div.style.float = "left";
       div.style.backgroundColor = "orange";
 
-
       div.addEventListener("mouseover", function() {
-        this.style.backgroundColor = " green";
+        this.style.backgroundColor = " blue";
         console.log(this);
       });
 
@@ -24,8 +22,34 @@ function setGrid(n, m) {
 }
 
 
+function colorGrid(a,b) {
+  var swatchGrid = document.getElementById('swatchGrid');
+  swatchGrid.setAttribute('style',`width: ${a*20}px; height: ${b*20}px;` );
 
-setGrid(20,20);
+  for (var i = 0; i < a; i++) {
+    for (var x = 0; x < b; x++) {
+      var div = document.createElement("div");
+      div.classList.add('cells');
+      div.id = 'div' + x; // remove! -ray
+      swatchGrid.appendChild(div);
+      div.style.float = "left";
+      div.style.backgroundColor = "white";
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+setGrid(30,30);
+colorGrid(6,15);
+
 
 
 
